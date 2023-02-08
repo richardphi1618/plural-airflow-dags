9af36fb3a6ec87ad19ef57fe28f6c7b0dfd0d9c3
Richard Blanchette <richardphi1618@gmail.com>
2023-02-08 12:50:43 -0500

    fix: make a minor change to see state change in deployment

--- foobar_dag.py
+++ foobar_dag.py
@@ -6,25 +6,11 @@ from airflow.utils.dates import days_ago
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    "owner": "todd",
+    "owner": "nobody",
     "depends_on_past": False,
-    "email": ["todd.koym@apexcleanenergy.com"],
     "email_on_failure": True,
     "email_on_retry": False,
     "retries": 0,
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
 }
 dag = DAG(
     "foobar",
