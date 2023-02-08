525f423ae8a035048168935b7602e8ea0d31930d
Richard Blanchette <richardphi1618@gmail.com>
2023-02-08 12:54:44 -0500

    fix: dag name

--- look_todd_new_dag.py
+++ look_todd_new_dag.py
@@ -32,7 +32,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 with DAG(
-    'tutorial',
+    'look_todd_new_dag',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(minutes=30),
